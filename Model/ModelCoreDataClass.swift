76804424823cc8ca392dffc75db398f61174549b
Yzee <yan.zhigurs@mail.ru>
2023-04-05 18:12:16 +0400

    create CoreData

---
+++ Model/ModelCoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Users+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Ян Жигурс on 05.04.2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Users)
+public class Users: NSManagedObject {
+
+}
